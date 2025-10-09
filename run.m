45f7b5ce77aa284d50a80afeacddcc34ae49adc1
tangchuan2000 <105832166+tangchuan2000@users.noreply.github.com>
2025-10-09 09:10:36 +0800

    Update run.m

--- run.m
+++ run.m
@@ -29,30 +29,19 @@ for dsi = 1:dbNum
     num_view = length(X);
     
     %% para setting
-    anchor = [1,2,4]*k ;
-    d = [1,2,4]*k ;
-    lamb = [0.0001,0.001, 0.01,0.1,1];
-
-    maxAcc = 0;
-    maxStr = '';
-    for i_m = 1:length(anchor)
-        for i_d = 1:length(d)
-            for i_lamb = 1:length(lamb)                
-                [index] = SLWSE(X,Y,d(i_d),anchor(i_m), lamb(i_lamb)); % X,Y,lambda,d,numanchor
-                res = Clustering8Measure(Y, index); % ACC nmi AR Fscore Purity  Precision Recall
-                str = sprintf('db:%s\t  ACC:%.4f nmi:%.4f AR:%.4f Fscore:%.4f Purity:%.4f  Precision:%.4f Recall:%.4f \n',...
-                    dataName,  res(1), res(2), res(3), res(4), res(5), res(6), res(7));
-                fprintf(str);
-                clear index;
-                if (maxAcc < res(1))
-                    maxAcc = res(1);
-                    maxStr = str;
-                end
-            end
-        end
-    end
-    fprintf('Max:%s', maxStr);
-    clear X Y k;
+    anchor_list = [1,2,4]*k ;
+    d_list = [1,2,4]*k ;
+    lamb_list = [0.0001,0.001, 0.01,0.1,1];
+    %%%%% for Caltech101-20 %%%%%%%%%%%%%
+    d = d_list(2);
+    m = anchor_list(1);
+    lambda = lamb_list(4);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    [index] = SLWSE(X,Y,d,m, lambda); % X,Y,lambda,d,numanchor
+    res = Clustering8Measure(Y, index); % ACC nmi AR Fscore Purity  Precision Recall
+    str = sprintf('db:%s\t  ACC:%.4f nmi:%.4f AR:%.4f Fscore:%.4f Purity:%.4f  Precision:%.4f Recall:%.4f \n',...
+        dataName,  res(1), res(2), res(3), res(4), res(5), res(6), res(7));
+    fprintf(str);
 end
 
 
